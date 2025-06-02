10324419285f624d057d2cd2f47c72381a0eaf32
armatree2 <aramtree2@gmail.com>
2025-06-02 22:00:36 +0900

    mml parser

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
 import moo from 'https://cdn.skypack.dev/moo';
 
 var mml = document.getElementById("mml");
-var result = document.getElementById("result");
+var result = document.getElementById("LexResult");
+var submit = document.getElementById("submit");
 
-const lexer = moo.compile({
-    Note: /[a-g]/,
-    Num: /0|[1-9][0-9]{0,2}/
-});
+let lexer = moo.compile({
+    WS: /[ \t]+/,
+    Tempo: /[Tt](?:0|[1-9][0-9]{0,2})/,
+    Octave: /[Oo](?:0|[1-9][0-9]{0,2})/,
+    Volume: /[Vv](?:0|[1-9][0-9]{0,2})/,
+    Length: /[Ll](?:0|[1-9][0-9]{0,2})\.?/,
+    OctaveUp: /\>/,
+    OctaveDown: /\</,
+    Note: /[a-gA-G][\+\-]?(?:0|[1-9][0-9]{0,2})?\.?/,
+    AbsNote: /[Nn](?:0|[1-9][0-9]{0,2})/,
+    Rest: /[Rr](?:0|[1-9][0-9]{0,2})?\.?/,
+    tie: /&/,
+})
 
+submit.addEventListener("click",()=>{
+    parse();
+})
 
-lexer.reset("c d 123 001");
-result.innerText = "MML Parse Reult \n"
-for (let token of lexer)
+function parse()
 {
-    result.innerText += token;
-    result.innerText += "\n";
+    lexer.reset(mml.value);
+    result.innerText = "MML Parse Reult \n"
+    var token = lexer.next();
+    while(token != null)
+    {
+        console.log(token);
+        result.innerText += `Type : ${token.type} , Value : ${token.value}`;
+        result.innerText += "\n";
+        var token = lexer.next();
+    }
 }
